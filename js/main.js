f23a5a8d7197ced8a580279d67222647f5f14ae6
dinaahmed4 <100288547+dinaahmed4@users.noreply.github.com>
2022-12-19 17:42:33 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//change  h2 
+
+document.getElementById("example").innerHTML = "Hello from extarnel file Dina";
+
+document.getElementById("example").style.color = "red";
+
+document.getElementById("example").style.textAlign = "center";
+
+// alert
+
+alert("Hello from extarnal file");
+
+//console
+
+console.log("dina ahmed");
+
+//document.write
+
+document.write("nice");
+
+
+
